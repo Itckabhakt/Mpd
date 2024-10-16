4eb1ae4d981c8dcd9170f2cd1b3ddb1ef7704abd
Itckabhakt <165606700+Itckabhakt@users.noreply.github.com>
2024-10-16 14:33:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ const proxyServer = createServer((req, res) => {
   const options = {
     headers: {
       "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36",
+      "Referer": "https://mpd-six.vercel.app/",
+      "Origin": "https://mpd-six.vercel.app/"
     },
   };
 
